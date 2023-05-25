2139c62c14aa30b5b90f153a6a4ce5895e8b2a82
BurCha <1477996221@qq.com>
2023-05-25 18:31:44 +0800

    fix: 完善接口mock数据

--- src/apis/map.js
+++ src/apis/map.js
@@ -4,20 +4,20 @@ import axios from 'axios'
 export const getGeoJson = (type, name) => {
   if (type === 'country') return axios.get('/geoJsonColl/china.json')
   if (type === 'province') return axios.get(`/geoJsonColl/province/${name}.json`)
-  // 判断是区，县，获取相应数据
   return axios.get(`/geoJsonColl/city/${name}.json`)
 }
 
+// 获取中国数据
 export const getChinaData = () => {
   return axios.get('/mock/china.json')
 }
 
+// 获取省、直辖市、自治区的数据（山东省）
 export const getProvinceData = () => {
-  // 判断是省，自治区，直辖市，获取相应数据
   return axios.get('/mock/province.json')
 }
 
+// 获取区、县的数据（济南市）
 export const getCityData = () => {
-  // 判断是市，直辖市，获取相应数据
   return axios.get('/mock/city.json')
 }
