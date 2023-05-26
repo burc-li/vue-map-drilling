c7b9d7168187aad35d7bd6dc831011d6670f4112
BurCha <1477996221@qq.com>
2023-05-26 17:46:29 +0800

    fix: 优化常量颗粒度

--- src/components/MapDrilling/constant.js
+++ src/components/MapDrilling/constant.js
@@ -1,73 +1,147 @@
-// 34个省、市、自治区的名字拼音映射数组
-export const PROVINCES = [
-  // 23个省
-  '台湾',
-  '河北',
-  '山西',
-  '辽宁',
-  '吉林',
-  '黑龙江',
-  '江苏',
-  '浙江',
-  '安徽',
-  '福建',
-  '江西',
-  '山东',
-  '河南',
-  '湖北',
-  '湖南',
-  '广东',
-  '海南',
-  '四川',
-  '贵州',
-  '云南',
-  '陕西',
-  '甘肃',
-  '青海',
-  // 5个自治区
-  '新疆',
-  '广西',
-  '内蒙古',
-  '宁夏',
-  '西藏',
-  // 4个直辖市
-  '北京',
-  '天津',
-  '上海',
-  '重庆',
-  // 2个特别行政区
-  '香港',
-  '澳门',
-]
-// 直辖市和特别行政区-只有二级地图，没有三级地图
-export const SPECIALPROVINCES = ['北京', '天津', '上海', '重庆', '香港', '澳门', '台湾']
-// 点击县级区域，其 seriesName 格式为 '省级-市级'，无法下钻四级地图
-export const COUNTY = [
-  '河北-',
-  '山西-',
-  '辽宁-',
-  '吉林-',
-  '黑龙江-',
-  '江苏-',
-  '浙江-',
-  '安徽-',
-  '福建-',
-  '江西-',
-  '山东-',
-  '河南-',
-  '湖北-',
-  '湖南-',
-  '广东-',
-  '海南-',
-  '四川-',
-  '贵州-',
-  '云南-',
-  '陕西-',
-  '甘肃-',
-  '青海-',
-  '新疆-',
-  '广西-',
-  '内蒙古-',
-  '宁夏-',
-  '西藏-',
-]
+// 23个省
+const PROVINCE = [
+  {
+    adcode: 130000,
+    name: '河北',
+  },
+  {
+    adcode: 140000,
+    name: '山西',
+  },
+  {
+    adcode: 210000,
+    name: '辽宁',
+  },
+  {
+    adcode: 220000,
+    name: '吉林',
+  },
+  {
+    adcode: 230000,
+    name: '黑龙江',
+  },
+  {
+    adcode: 320000,
+    name: '江苏',
+  },
+  {
+    adcode: 330000,
+    name: '浙江',
+  },
+  {
+    adcode: 340000,
+    name: '安徽',
+  },
+  {
+    adcode: 350000,
+    name: '福建',
+  },
+  {
+    adcode: 360000,
+    name: '江西',
+  },
+  {
+    adcode: 370000,
+    name: '山东',
+  },
+  {
+    adcode: 410000,
+    name: '河南',
+  },
+  {
+    adcode: 420000,
+    name: '湖北',
+  },
+  {
+    adcode: 430000,
+    name: '湖南',
+  },
+  {
+    adcode: 440000,
+    name: '广东',
+  },
+  {
+    adcode: 460000,
+    name: '海南',
+  },
+  {
+    adcode: 510000,
+    name: '四川',
+  },
+  {
+    adcode: 520000,
+    name: '贵州',
+  },
+  {
+    adcode: 530000,
+    name: '云南',
+  },
+  {
+    adcode: 610000,
+    name: '陕西',
+  },
+  {
+    adcode: 620000,
+    name: '甘肃',
+  },
+  {
+    adcode: 630000,
+    name: '青海',
+  },
+  {
+    adcode: 710000,
+    name: '台湾',
+  }]
+
+// 5个自治区
+const AUTONOMOUS_REGION = [{
+  adcode: 150000,
+  name: '内蒙古',
+}, {
+  adcode: 450000,
+  name: '广西',
+}, {
+  adcode: 540000,
+  name: '西藏',
+}, {
+  adcode: 640000,
+  name: '宁夏',
+}, {
+  adcode: 650000,
+  name: '新疆',
+}]
+
+// 4个直辖市 (centrally administered municipality)
+const CAM = [{
+  adcode: 110000,
+  name: '北京',
+}, {
+  adcode: 120000,
+  name: '天津',
+}, {
+  adcode: 310000,
+  name: '上海',
+}, {
+  adcode: 500000,
+  name: '重庆',
+}]
+
+// 2个特别行政区 (special administrative region)
+const SAR = [{
+  adcode: 810000,
+  name: '香港',
+}, {
+  adcode: 820000,
+  name: '澳门',
+}]
+
+// 有二级地图 （23个省、5个自治区、4个直辖市、2个特别行政区）
+export const EXISTING_SECOND_LAYER_REGION = [...PROVINCE, ...AUTONOMOUS_REGION, ...CAM, ...SAR]
+
+// 有三级地图 （23个省、5个自治区 的市级区域）
+export const EXISTING_THIRD_LAYER_REGION = [...PROVINCE, ...AUTONOMOUS_REGION]
+
+// 九段线 adcode
+export const JD_ADCODE = 100000
+// 中国台湾省 adcode
+export const TAIWAN_ADCODE = 710000
