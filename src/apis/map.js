64831fe4a076cccc3901ee61ade31d2daf80820f
BurCha <1477996221@qq.com>
2023-05-26 17:53:16 +0800

    fix: 合成接口

--- src/apis/map.js
+++ src/apis/map.js
@@ -2,22 +2,24 @@ import axios from 'axios'
 
 // 获取GeoJSON数据
 export const getGeoJson = (type, name) => {
-  if (type === 'country') return axios.get('/geoJsonColl/china.json')
+  // 获取中国的geojson数据
+  if (type === 'china') return axios.get('/geoJsonColl/china.json')
+
+  // 获取省、直辖市、自治区的geojson数据
   if (type === 'province') return axios.get(`/geoJsonColl/province/${name}.json`)
+
+  // 获取区、县的geojson数据
   return axios.get(`/geoJsonColl/city/${name}.json`)
 }
 
-// 获取中国数据
-export const getChinaData = () => {
-  return axios.get('/mock/china.json')
-}
+// 获取地图数据
+export const getMapData = (type) => {
+  // 获取中国数据
+  if (type === 'china') return axios.get('/mock/china.json')
 
-// 获取省、直辖市、自治区的数据（山东省）
-export const getProvinceData = () => {
-  return axios.get('/mock/province.json')
-}
+  // 获取省、直辖市、自治区的数据（山东省）
+  if (type === 'province') return axios.get('/mock/province.json')
 
-// 获取区、县的数据（济南市）
-export const getCityData = () => {
+  // 获取区、县的数据（济南市）
   return axios.get('/mock/city.json')
 }
